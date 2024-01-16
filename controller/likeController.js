a03e38c8fcb1c8d62c24802163b8db0965e9c1ef
Munseok Song <song3392@gmail.com>
2024-01-16 15:59:03 +0900

    좋아요 API : JWT Token Authorization 추가

--- controller/likeController.js
+++ controller/likeController.js
@@ -1,12 +1,24 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+let jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const ensureAuthorization = (req) => {
+  let receivedJwt = req.headers['authorization'];
+
+  let authorization = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+
+  return authorization;
+};
 
 const addLike = (req, res) => {
   const { bookId } = req.params;
-  const { userId } = req.body;
+
+  let authorization = ensureAuthorization(req);
 
   let sql = 'INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)';
-  let values = [userId, bookId];
+  let values = [authorization.id, bookId];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -19,10 +31,11 @@ const addLike = (req, res) => {
 
 const removeLike = (req, res) => {
   const { bookId } = req.params;
-  const { userId } = req.body;
+
+  let decodedJwt = ensureAuthorization(req);
 
   let sql = 'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?';
-  let values = [userId, bookId];
+  let values = [decodedJwt.id, bookId];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
